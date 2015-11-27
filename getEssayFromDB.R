6b4d722cdbdf99c4a574e68fddcca76fd8f15b3a
Dr. S. Keem <keem@keem.net>
2015-11-26 20:41:05 -0500

    Intermediate

--- getEssayFromDB.R
+++ getEssayFromDB.R
@@ -5,14 +5,14 @@
 
 getEssayFeatureFromDB <- function() {
      library(RMySQL)
-     essayDB = dbConnect(MySQL(), user='keem', password='zmxncb1', dbname='nlp',host='fc.keem.net')
+     essayDB = dbConnect(MySQL(), user='keem', password='Xzmxncb1', dbname='nlp',host='fc.keem.net')
      result = dbSendQuery(essayDB, "select * from nlp1")
      essays = fetch(result, n=-1)
 }
 
 getEssayScoreFromDB <- function() {
      library(RMySQL)
-     essayDB = dbConnect(MySQL(), user='keem', password='zmxncb1', dbname='nlp',host='fc.keem.net')
+     essayDB = dbConnect(MySQL(), user='keem', password='Xzmxncb1', dbname='nlp',host='fc.keem.net')
      result = dbSendQuery(essayDB, "select * from nlp3")
      essays = fetch(result, n=-1)
      
--- main.R
+++ main.R
@@ -2,8 +2,11 @@ features<-getEssayFeatureFromDB()
 scores<-getEssayScoreFromDB()
 adf<-getPOSCounting(features)
 fns<-merge(adf, scores, by="essayNo")
+fns$rate = round(fns$rate*2)
 #fns_sorted<-fns[order(fns$essayNo),]
 fns_group<-split(fns, fns$type)
 table(fns$type)
 gre<-fns_group$GRE
 gre$essayNo<-as.integer(gre$essayNo)
+toefl<-fns_group$TOEFL
+toefl_score_group<-table(toefl$score)
